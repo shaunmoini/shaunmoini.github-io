b2f8035136d10e1c02d1c4641a4c1b891a010ea6
shaunmoini <primary.axos@dralias.com>
2022-02-09 18:37:30 -0500

    Hero banner implemented. Navbar slide in and out on scroll. Containers all use is-max-desktop (container max width is 960px) Added nord.sass, animate.css, and hover.css.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
+  // All animations from animate.css will take half the time to accomplish
+  document.documentElement.style.setProperty('--animate-duration', '.5s');
 
+  //navbar event listeners
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(() => {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
@@ -8,9 +11,22 @@ $(document).ready(() => {
   });
 
   // Check for click events on navbar-items
-  $(".navbar-item").click(() => {
+  $(".navbar-end .navbar-item").click(() => {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
+  });
+
+  // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
+  let prevScrollpos = window.pageYOffset;
+  $(window).scroll(() => {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      $("nav").removeClass("animate__slideOutUp");
+      $("nav").addClass("animate__slideInDown");
+    }
+    else
+      $("nav").addClass("animate__animated animate__slideOutUp");
+    prevScrollpos = currentScrollPos;
   });
 });
