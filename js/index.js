150a64edce35ac5d549c2afe7ee9ec9018f97449
shaunmoini <primary.axos@dralias.com>
2022-03-02 16:20:51 -0500

    Navbar background colour and event listeners corrected. Fadein animations completed for all sections. Home button colour updated.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+  // event listeners
+
   // theme toggle
   let themeToggle = $("#theme-toggle");
   let themeLink = $("#theme");
@@ -15,16 +17,8 @@ $(document).ready(() => {
 
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(() => {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
-
-  // Check for click events on navbar-items
-  $(".navbar-end .navbar-item").click(() => {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
+    $(".navbar-menu").slideToggle(300);
   });
 
   // contact form submit button
@@ -36,6 +30,36 @@ $(document).ready(() => {
     // clear form after submission
     $("#form-subject").val('');
     $("#form-body").val('');
+  });
+
+  // animations and project section
+
+  // fade in animations
+  if ($(window).scrollTop() < 200) {
+    $("#home-title p:nth-child(1)").delay(500).fadeTo(300, 1);
+    $("#home-title p:nth-child(2)").delay(1100).fadeTo(300, 1);
+    $("#home-title p:nth-child(3)").delay(1700).fadeTo(300, 1);
+    $("#outgoing-links-home").delay(2300).fadeTo(300, 1);
+    $(".navbar").delay(2300).fadeTo(300, 1);
+  }
+  else {
+    $("#home-title p:nth-child(1)").fadeTo(0, 1);
+    $("#home-title p:nth-child(2)").fadeTo(0, 1);
+    $("#home-title p:nth-child(3)").fadeTo(0, 1);
+    $("#outgoing-links-home").fadeTo(0, 1);
+    $(".navbar").fadeTo(0, 1);
+  }
+
+  // on page load, if sections are visible then show
+  if ($("#about").visible(true)) $("#about .container").delay(500).fadeTo(300, 1);
+  if ($("#project").visible(true)) $("#project .container").delay(500).fadeTo(300, 1);
+  if ($("#contact").visible(true)) $("#contact .container").delay(500).fadeTo(300, 1);
+
+  // on scroll, if sections are visible then show
+  $(window).on("resize scroll", () => {
+    if ($("#about").visible(true)) $("#about .container").delay(500).fadeTo(300, 1);
+    if ($("#project").visible(true)) $("#project .container").delay(500).fadeTo(300, 1);
+    if ($("#contact").visible(true)) $("#contact .container").delay(500).fadeTo(300, 1);
   });
 
   // populating project section using github api
@@ -70,11 +94,4 @@ $(document).ready(() => {
       projectSection.append(row);
     }
   });
-
-  // fade in animations
-  $("#home-title p:nth-child(1)").delay(500).fadeTo(300, 1);
-  $("#home-title p:nth-child(2)").delay(1100).fadeTo(300, 1);
-  $("#home-title p:nth-child(3)").delay(1700).fadeTo(300, 1);
-  $("#outgoing-links-home").delay(2300).fadeTo(300, 1);
-  $("#navbar").delay(2300).fadeTo(300, 1);
 });
